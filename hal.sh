7f5791ff0b89473cc9e5d9b078a9d33c9b6975ca
Gandalf- <austin.voecks@gmail.com>
2016-07-17 21:15:56 -0700

    Add home setting, remember phrase and recall phrase

--- hal.sh
+++ hal.sh
@@ -4,6 +4,52 @@
 log_file='/home/minecraft/logs/latest.log'
 memory_dir='/tmp/hal/'
 
+
+function tell_joke(){
+  : ' none -> none
+  tell a random joke
+  '
+  say "$(random \
+  'How does Steve get his exercise?  He runs around the block. ' \
+  'Have you heard of the creeper that went to a party?  He had a BLAST!' \
+  'Whats a ghasts favorite country?  The Nether-Lands!' \
+  'Why did the Creeper cross the road?  To get to the other Sssssssside!' \
+  'Why did the sailor bring iron and gold into his boat?  He needed oars.' \
+  'If there will ever be a Minecraft movie, then it would be a blockbuster.' \
+  'Why cant the Ender Dragon read a book?  Because he always starts at the End.' \
+  'Why dont blazes ever make businesses?  They keep firing people! ' \
+  'What is the national sport of Minecraft?  Boxing.' \
+  'What did Steve say to his girlfriend?  I dig you.' \
+  'What kind of parties do Minecraft players have?  Block parties. ' \
+  'A creeper walks into a bar. Everyone dies.' \
+  'When I saw the guy with a potion I knew there was trouble brewing.' \
+  'Id tell you a joke about the end, but it will just dragon.' \
+  'How do you make people change direction in Minecraft?  You Block their path.' \
+  'Why would a mushroom make a good roommate?  Its a real fungi.' \
+  'Whats Cobblestones favorite music?  Rock music.' \
+  'What did the chicken say to the cow? Pleased to meat you.' \
+  'What did the chicken say to the sheep? Pleased to meet ewe.' \
+  'What did the chicken say to the ocean? Nothing, it just waved.' \
+  'Did you hear about the murder of the snow golem?  It became a cold case.' \
+  'What do you get if you push a music box down a mineshaft?  A flat minor.' \
+  'How does Herobrine spy on people?  He uses spy-ders.' \
+  'Endermen scare people out if their mines.' \
+  'After I took the wool off a sheep, it told me, Sheariously?' \
+  'Why did the creeper cross the road?  There was an ocelot chasing him.' \
+  'What did the minecraft turkey say?  cobble, cobble, cobble! ' \
+  'Whats so good about cobblestone?  Its Hand-PICKED.' \
+  'What is a creepers favorite subject?  HisssSSSSStory' \
+  'I heard Minecraft Steve isnt very good at thinking outside of the box.' \
+  'Why couldnt the minecraft player go to the bar?  Because he was a miner.' \
+  'An Insult:  Your IQ is lower than bedrock.' \
+  'How good is Minecraft?  Top-Notch!' \
+  'Why did the enderman cross the road?  He didn’t, he Teleported.' \
+  'Whats an endermans favourite band?  Imagine Dragons!' \
+  'How does Steve chop down trees with his fists?  How wood I know?' \
+  'What is a pigmans favorite cereal?  Golden nuggets.')"
+  ran_command=0
+}
+
 function hc(){
   : ' string -> int
   check if the current line contains the required text and the "hal" keyword
@@ -36,6 +82,15 @@ function say(){
   '
   if test "$quiet" == "0" ; then
     tmux send-keys -t minecraft "/say [Hal] $1" Enter
+  fi
+}
+
+function tell(){
+  : ' string -> none
+  say a phrase in the server
+  '
+  if test "$quiet" == "0" ; then
+    tmux send-keys -t minecraft "/tell $user $1" Enter
   fi
 }
 
@@ -72,14 +127,20 @@ function random_okay(){
   : ' string -> string
   returns a random affirmative
   '
-  echo "$(random "$1" 'Okay!' 'Sure!' 'You got it!' 'Why not!' 'As you wish!' 'Done!')"
+  echo $(random \
+    "$1" 'Okay!' 'Sure!' 'You got it!' 'Why not!' 'As you wish!' 'Done!')
 }
 
 function random_musing(){
   : ' none -> string
   returns a random musing
   '
-  echo "$(random 'Hmm... I wonder...' 'All systems normal...' 'Counting sheep...' 'Just growing some trees...' 'Reorganizing clouds...' 'Turning dirt to grass...' 'Mind controlling a squid...' 'Hiding diamonds...' 'Looking for lost cows...' 'Did you know about overviewer.anardil.net?')"
+  echo $(random \
+    'Hmm... I wonder...' 'All systems normal...' 'Counting sheep...' 
+    'Just growing some trees...' 'Reorganizing clouds...' \ 
+    'Turning dirt to grass...' 'Mind controlling a squid...' \ 
+    'Hiding diamonds...' 'Looking for lost cows...' \ 
+    'Did you know about overviewer.anardil.net?' )
 }
 
 function shut_down(){
@@ -125,7 +186,7 @@ function set_home(){
   : ' none -> none
   attempts to set the current users home destination
   '
-  homeline=$(echo "$currline" | grep -oh 'set home as .*$')
+  homeline=$(echo "$currline" | grep -ioh 'set home as .*$')
   xcoord=$(echo "$homeline" | cut -f 4 -d ' ')
   ycoord=$(echo "$homeline" | cut -f 5 -d ' ')
   zcoord=$(echo "$homeline" | cut -f 6 -d ' ')
@@ -138,6 +199,30 @@ function set_home(){
     say "Okay $user, I've set your home to be $xcoord $ycoord $zcoord!"
   fi
   ran_command=0
+}
+
+function remember_phrase(){
+  : ' none -> none
+  parse out note to remember and write to user file
+  '
+  say "Okay $user, I'll remember!"
+  note=$(echo "$currline" | grep -oih 'remember .*$' | cut -f 2- -d ' ')
+  echo "$note" >> "$memory_dir""$user".memories
+  ran_command=0
+}
+
+function recall_phrase(){
+  : ' none -> none
+  search through user memories for related information
+  '
+  phrase=$(echo $currline | grep -oih 'recall .*$' | cut -f 2- -d ' ')
+  mem_file="$memory_dir""$user".memories
+  ran_command=0
+
+  say "Okay $user, here's what I know about $phrase:"
+  for memory in "$(cat $mem_file | grep "$phrase")"; do
+    say "\"$memory\""
+  done
 }
 
 currline=""
@@ -154,6 +239,7 @@ starttime=$(date +%s)
 mkdir -p "$memory_dir"
 sleep 1
 
+# main
 while inotifywait -e modify $log_file; do
 
   # preparation
@@ -188,13 +274,13 @@ while inotifywait -e modify $log_file; do
     if $(hc "help"); then
       say "I'm Hal, a teenie tiny AI that will try to help you!"
       say "Here are somethings I understand:"
-      say "- hello, hey, how are you, what's up"
+      say "- hello, hey, how are you, what's up, tell a joke"
       say "- thanks, yes, no, whatever"
       say "- help, restart, be quiet, you can talk, status update"
       say "- make it (day, night, clear, rainy)"
       say "- make me (healthy, invisible, fast)"
-      say "- take me (to the telehub, home)"
-      say "- set home as <x> <y> <z>"
+      say "- take me (to the telehub, home), set home as <x> <y> <z>"
+      say "- remember <phrase>, recall <phrase>, forget everything"
       say "- put me in (creative, survival, spectator) mode"
       ran_command=0
     fi
@@ -240,6 +326,24 @@ while inotifywait -e modify $log_file; do
     hcsr "what's up" \
       "Not too much $user! Just $(random 'holding the world together' 'hanging out' 'mind controlling a squid' 'contemplating the universe')"
 
+    if $(hc 'tell me a joke'); then tell_joke ; fi
+    if $(hc 'tell a joke'); then tell_joke ; fi
+
+    # memory
+    if $(hc 'remember'); then
+      remember_phrase
+    fi
+
+    if $(hc 'recall'); then
+      recall_phrase
+    fi
+    
+    if $(hc 'forget everything'); then
+      say "Done $user, I forgot everything"
+      echo "" > "$memory_dir""$user".memories
+      ran_command=0
+    fi
+
     # effects
     hcsr 'make me healthy' \
       "$(random_okay 'This should help you feel better')" \
@@ -258,13 +362,8 @@ while inotifywait -e modify $log_file; do
       "$(random_okay 'Off you go!')" \
       "/tp $user -108 3 98"
 
-    if $(hc 'take me home'); then
-      go_home
-    fi
-
-    if $(hc 'set home as'); then
-      set_home
-    fi
+    if $(hc 'take me home'); then go_home  ; fi
+    if $(hc 'set home as') ; then set_home ; fi
 
     # gamemode
     hcsr 'put me in survival mode' \
@@ -331,8 +430,8 @@ while inotifywait -e modify $log_file; do
 
     # misc server triggered
     if $(contains "$user moved too quickly"); then
-      say "Woah there $user! Maybe slow down a little?!"
       ran_command=0
+      say "Woah there $user! Maybe slow down a little?!"
     fi
 
     # not sure
@@ -341,6 +440,5 @@ while inotifywait -e modify $log_file; do
     fi
 
   fi
-
   prevline="$currline"
 done
