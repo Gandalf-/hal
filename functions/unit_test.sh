5486041d722d50fe60569e5023ba851e6944b595
Gandalf- <austin.voecks@gmail.com>
2016-07-26 21:33:03 -0700

    Add skeleton for rest of unit tests

--- functions/unit_test.sh
+++ functions/unit_test.sh
@@ -1,58 +1,145 @@
 #!/bin/bash
 
 source functions.sh
+debug=1
 
 # boilerplate
 #==================
-function pass(){ echo "$1: pass"; }
-function fail(){ echo "$1: fail"; }
+function pass(){ echo -n " pass"; }
+function fail(){ echo -n " fail"; }
 function scpass(){
-  if test "$2" == "$3"; then pass $1; else fail $1; fi
+  if test "$1" == "$2"; then pass; else fail; fi
 }
 function scfail(){
-  if test "$2" == "$3"; then fail $1; else pass $1; fi
+  if test "$1" == "$2"; then fail; else pass; fi
 }
 function rcpass(){
-  if test "$(echo "$2" | grep "$3")" != ""; then pass $1;
-  else fail $1; fi
+  if test "$(echo "$1" | grep "$2")" != ""; then pass;
+  else fail; fi
 }
 function rcfail(){
-  if test "$(echo "$2" | grep "$3")" != ""; then fail $1;
-  else pass $1; fi
+  if test "$(echo "$1" | grep "$2")" != ""; then fail;
+  else pass; fi
 }
 function ocpass(){
-  if [[ $? -eq 0 ]]; then pass $1; else fail $1; fi
+  if [[ $? -eq 0 ]]; then pass; else fail; fi
 }
 function ocfail(){
-  if [[ $? -eq 0 ]]; then fail $1; else pass $1; fi
+  if [[ $? -eq 0 ]]; then fail; else pass; fi
 }
 function test_test(){
-  scpass 'test' 'a' 'a'
-  rcpass 'test' 'apple blueberry watermelon' 'blue'
-  ocpass 'test' 0
-  scfail 'test' 'a' 'b'
-  rcfail 'test' 'apple blueberry watermelon' 'green'
-  ocfail 'test' 1
+  echo -n 'test            '
+  scpass 'a' 'a'
+  rcpass 'apple blueberry watermelon' 'blue'
+  true
+  ocpass 
+  scfail 'a' 'b'
+  rcfail 'apple blueberry watermelon' 'green'
+  false
+  ocfail 
   echo
 }
 
 # tests
 #==================
 function test_tell_joke() {
-  pass 'tell_joke'
+  : ' none -> none
+  make sure tell_joke returns a string
+  '
+  echo -n 'tell_joke       '
+  scfail tell_joke ""
   echo
 }
 
 function test_hc(){
+  : ' none -> none
+  make sure hc only accepts inputs that match "hal" and $1
+  '
+  echo -n 'hc              '
   declare -a arry=('hal blah' 'hal blah blah' 'blah HAL' 'blah hAl blah')
   for currline in "${arry[@]}"; do
-    ocpass 'hc' $(hc 'blah')
+    ocpass $(hc 'blah')
   done
 
   declare -a arry=('blah' 'hal herp' 'HAL' 'herp')
   for currline in "${arry[@]}"; do
-    ocfail 'hc' $(hc 'blah')
+    ocfail $(hc 'blah')
   done
+  echo
+}
+
+function test_contains(){
+  echo -n 'contains        '
+  echo
+}
+
+function test_say(){
+  echo -n 'say             '
+  echo
+}
+
+function test_tell(){
+  echo -n 'tell            '
+  echo
+}
+
+function test_run(){
+  echo -n 'run             '
+  echo
+}
+
+function test_not_repeat(){
+  echo -n 'not_repeat      '
+  echo
+}
+
+function test_random(){
+  echo -n 'random          '
+  echo
+}
+
+function test_random_okay(){
+  echo -n 'random_okay     '
+  echo
+}
+
+function test_random_musing(){
+  echo -n 'random_musing   '
+  echo
+}
+
+function test_shut_down(){
+  echo -n 'shut_down       '
+  echo
+}
+
+function test_hcsr(){
+  echo -n 'hcsr            '
+  echo
+}
+
+function test_go_home(){
+  echo -n 'go_home         '
+  echo
+}
+
+function test_set_home(){
+  echo -n 'set_home        '
+  echo
+}
+
+function test_remember_phrase(){
+  echo -n 'remember_phrase '
+  echo
+}
+
+function test_recall_phrase(){
+  echo -n 'recall_phrase   '
+  echo
+}
+
+function test_forget_phrase(){
+  echo -n 'forget_phrase   '
   echo
 }
 
@@ -61,3 +148,18 @@ function test_hc(){
 test_test
 test_tell_joke
 test_hc
+test_contains
+test_say
+test_tell
+test_run
+test_not_repeat
+test_random
+test_random_okay
+test_random_musing
+test_shut_down
+test_hcsr
+test_go_home
+test_set_home
+test_remember_phrase
+test_recall_phrase
+test_forget_phrase
