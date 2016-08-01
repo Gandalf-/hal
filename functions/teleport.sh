4f9a58b6ef574a92078d24e1b5bf1653274d2486
Gandalf- <austin.voecks@gmail.com>
2016-07-31 17:25:53 -0700

    Fixes for teleport.sh after shellcheck

--- functions/teleport.sh
+++ functions/teleport.sh
@@ -14,14 +14,14 @@ function go_to_dest(){
   local where=$(\
     echo "$currline" | grep -oih 'take me to .*$' |
     sed -e 's/\(hal\)//gI' -e 's/^[[:space:]]*$//' -e 's/[[:space:]]*$//' |
-    cut -f 4- -d ' ')
+    cut -f 4- -d ' ') || ''
 
   if test "$where" == ''; then
     say "Sorry $user, I don't know where that is!"
 
   else
     local dest=$(\
-      cat ~/.halrc | grep '\->' | grep -i "$where" | grep -oih '\->.*$' |
+      grep '\->' ~/.halrc | grep -i "$where" | grep -oih '\->.*$' |
       cut -f 2- -d ' ')
 
     if test "$dest" == '' ; then
@@ -33,16 +33,17 @@ function go_to_dest(){
       run "/tp $user $dest"
     fi
   fi
+  ran_command=0
 }
 
 function go_home(){
   : ' none -> none
   attempts to teleport the current user to their home destination
   '
-  local homeline=$(cat "$mem_dir""$user".home)
-  local xcoord=$(echo "$homeline" | cut -f 1 -d ' ')
-  local ycoord=$(echo "$homeline" | cut -f 2 -d ' ')
-  local zcoord=$(echo "$homeline" | cut -f 3 -d ' ')
+  local homeline=$(cat "$mem_dir""$user".home) || ''
+  local xcoord=$(echo "$homeline" | cut -f 1 -d ' ') || ''
+  local ycoord=$(echo "$homeline" | cut -f 2 -d ' ') || ''
+  local zcoord=$(echo "$homeline" | cut -f 3 -d ' ') || ''
 
   if test "$xcoord" == '' || test "$ycoord" == '' || test "$zcoord" == ''; then
     say "Sorry $user, either you never told me where home was or I forgot!"
@@ -57,10 +58,10 @@ function set_home(){
   : ' none -> none
   attempts to set the current users home destination
   '
-  local homeline=$(echo "$currline" | grep -ioh 'set home as .*$')
-  local xcoord=$(echo "$homeline" | cut -f 4 -d ' ')
-  local ycoord=$(echo "$homeline" | cut -f 5 -d ' ')
-  local zcoord=$(echo "$homeline" | cut -f 6 -d ' ')
+  local homeline=$(echo "$currline" | grep -ioh 'set home as .*$') || ''
+  local xcoord=$(echo "$homeline" | cut -f 4 -d ' ') || ''
+  local ycoord=$(echo "$homeline" | cut -f 5 -d ' ') || ''
+  local zcoord=$(echo "$homeline" | cut -f 6 -d ' ') || ''
 
   if test "$xcoord" == '' || test "$ycoord" == '' || test "$zcoord" == ''; then
     say "Sorry $user, something doesn't look right with those coordinates"
