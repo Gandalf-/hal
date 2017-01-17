5e208a03f54be5ecfcb125468a45cb1bbc269476
Gandalf- <austin.voecks@gmail.com>
2017-01-17 13:09:27 -0800

    Add quotes and braces around variables

--- demo/web_server.sh
+++ demo/web_server.sh
@@ -14,13 +14,13 @@ hal_pretty_print() {
   : ' string -> string
   Converts from hals Minecraft chat format to html format
   '
-  if test "$@" != ""; then
-    local message="$@"
+  if test "${@}" != ""; then
+    local message="${@}"
     message="$(sed 's/</\&lt/g' <<< "${message}")"
     message="$(sed 's/>/\&gt/g' <<< "${message}")"
     message="$(sed 's/\/say//g' <<< "${message}")"
     message="$(sed ':a;N;$!ba;s/\n/<br>/g' <<< "${message}")"
-    echo "$message<br>"
+    echo "${message}<br>"
   else
     echo ""
   fi
@@ -30,8 +30,8 @@ do_get() {
   : ' none -> none
   Handles GET requests, only the specified resources are returned
   '
-  echo -n "GET: $resource"
-  case $resource in
+  echo -n "GET: ${resource}"
+  case "${resource}" in
     /)
       ( echo -e "HTTP/1.1 200 OK\n"
       cat index.html
@@ -71,17 +71,17 @@ do_post() {
   '
   echo "POST"
   message=""
-  content="$(head -c $content_length incoming)"
-  echo "U -> S: \"$content\""
+  content="$(head -c ${content_length} incoming)"
+  echo "U -> S: \"${content}\""
 
   # log in
   if test "UUID" == ${content::4}; then
     name="$(cut -f 4 -d ' ' <<< "${content}")"
-    message="[$(date +"%H:%M:%S")] [Server thread/INFO]: $name joined the game"
+    message="[$(date +"%H:%M:%S")] [Server thread/INFO]: ${name} joined the game"
 
   # log out
   elif test "game" == $(tail -c 5 <<< ${content} ); then
-    message="[$(date +"%H:%M:%S")] [Server thread/INFO]: $content"
+    message="[$(date +"%H:%M:%S")] [Server thread/INFO]: ${content}"
 
   # chatting
   else
@@ -92,11 +92,11 @@ do_post() {
 
   # provide hal user input, timeout after 1 second
   echo "S -> H: ${message}"
-  echo "${message}" >> $hal_input_file
+  echo "${message}" >> "${hal_input_file}"
 
   # wait for hal to work, timeout after 1 second
   timeout=$(( $(date +'%s') + 1))
-  before_time=$(stat -c '%Z' $hal_output_file)
+  before_time=$(stat -c '%Z' "${hal_output_file}")
   current_time=$before_time
 
   while test $before_time -eq $current_time; do
@@ -127,26 +127,25 @@ cleanup() {
   '
   echo ""
   echo "Stopping server"
-  kill $server_pid
+  kill ${server_pid}
   echo "Stopping hal"
-  kill $hal_pid
+  kill ${hal_pid}
+  echo "Cleaning up filesystem"
+  rm -f outgoing incoming ${hal_input_file} ${hal_output_file}
   echo "Exiting"
-  rm -f outgoing incoming $hal_input_file $hal_output_file
   exit
 }
 
 # setup
 trap cleanup INT
-
-mkdir -p $root_dir
-touch $hal_output_file $hal_input_file
-
+mkdir -p ${root_dir}
+touch ${hal_output_file} ${hal_input_file}
 rm -f outgoing incoming
 mkfifo outgoing incoming
 
 # start hal
 echo "Starting hal..."
-bash ../hal.sh $hal_input_file ../ $root_dir $hal_output_file &
+bash ../hal.sh ${hal_input_file} ../ ${root_dir} ${hal_output_file} &
 hal_pid=$!
 
 # start http server
@@ -187,9 +186,6 @@ while true; do
       ;;
     POST)
       do_post
-      ;;
-    *)
-      echo "Unhandled method"
       ;;
   esac
 done
